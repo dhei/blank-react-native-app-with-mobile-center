96fb62a07ce7b532c0b007ed119ddb48317467b9
Di Hei <dihei@microsoft.com>
2017-09-26 09:33:52 -0700

    react-native link mobile-center packages

---
+++ ios/Pods/Target Support Files/Pods-blankapp-tvOS/Pods-blankapp-tvOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_blankapp_tvOS : NSObject
+@end
+@implementation PodsDummy_Pods_blankapp_tvOS
+@end
---
+++ ios/Pods/Target Support Files/Pods-blankapp/Pods-blankapp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_blankapp : NSObject
+@end
+@implementation PodsDummy_Pods_blankapp
+@end
